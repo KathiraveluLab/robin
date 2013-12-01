259e2b327545f6f09c9bae9cbe1a7bf8ddb75ac2
Pradeeban <pradeeban@llovizna.(none)>
2013-12-01 18:35:39 +0100

    Refactoring by removing the global variables and moving them as fields.

--- robin.js
+++ robin.js
@@ -2,101 +2,103 @@ var httpProxy = require('http-proxy/lib/node-http-proxy'),
     etc = require('etc'),
     Cookies = require('cookies');
 
-var configObject = etc().argv().env().etc();
-var conf = configObject.toJSON();
-
-var noOfDeployments = conf.deployments.length;
-var defaultDeployment, domainIndex, target;
 Robin.prototype.maximumRandomNumber = 1000;
 
-var cookies, receivedValue, cookieValue;
-
-var addresses = initAddresses();
-var pport = initProxyPort();
-var expiryTime = setExpiryTime();
-var labels = initLabels();
-
-var robin = new Robin();
-
-var server = httpProxy.createServer(function (req, res, proxy) {
-    cookies = new Cookies(req, res);
-    receivedValue = cookies.get(robin.cookieName);
-    if (receivedValue == undefined) {
-        target = robin.matchProxy(res);
-        cookieValue = labels[domainIndex];
-        cookies.set(robin.cookieName,cookieValue, {expires: expiryTime}, {domain: req.headers.host});
-        res.writeHead( 302, { "Location": "/" } )
-        return res.end();
-     } else {        
-        target = robin.findDeployment(receivedValue);
-     }
-    proxy.proxyRequest(req, res, target);
-}).listen(pport);
-
 function Robin() {
-    this.cookieName = conf.cookie_name;
+    this.configObject = etc().argv().env().etc();
+    this.conf = this.configObject.toJSON();
+    this.noOfDeployments = this.conf.deployments.length;
+    this.cookieName = this.conf.cookie_name;
+    this.cookies;
+    this.receivedValue;
+    this.cookieValue;
+    this.addresses = this.initAddresses();
+    this.pport = this.initProxyPort();
+    this.expiryTime = this.setExpiryTime();
+    this.labels = this.initLabels();
+    this.defaultDeployment; 
+    this.domainIndex;
+    this.target;
 }
 
-Robin.prototype.matchProxy = function (res) {
-    var randomnumber= this.generateRandomNumber();
-    var depWeight;
-    for (var i = 0; i < noOfDeployments; i++) {
-        depWeight = conf.deployments[i].weight;
-        if (randomnumber < depWeight) {
-            domainIndex = i;
-            return addresses[i];
-        } else {
-            randomnumber = randomnumber - depWeight;
-        }
-    }
-    return defaultDeployment;
-}
-
-Robin.prototype.findDeployment = function (depLabel) {
-    for (var i = 0; i < noOfDeployments; i++) {
-        if (depLabel == labels[i]) {
-            return addresses[i];
-        } 
-    }
-    return defaultDeployment;
-}
-
-Robin.prototype.generateRandomNumber = function () {
-    var randomNumber = Math.floor(Math.random()*(this.maximumRandomNumber+1));
-    return randomNumber;
-}
-
-function initAddresses() {
+Robin.prototype.initAddresses = function () {
     var deployments = [];
-    for (var i = 0; i < noOfDeployments; i ++) {
+    for (var i = 0; i < this.noOfDeployments; i ++) {
         deployments[i] = {
-            host: conf.deployments[i].addr,
-            port: conf.deployments[i].port
+            host: this.conf.deployments[i].addr,
+            port: this.conf.deployments[i].port
         };
-        if (conf.deployments[i].addr == conf.default_deployment) {
-            domainIndex = i;
-            defaultDeployment = deployments[i];
+        if (this.conf.deployments[i].addr == this.conf.default_deployment) {
+            this.domainIndex = i;
+            this.defaultDeployment = deployments[i];
         }
     }
     return deployments;
 }
 
-function initLabels() {
+Robin.prototype.initLabels = function () {
     var deploymentLabels = [];
-    for (var i = 0; i < noOfDeployments; i ++) {
-        deploymentLabels[i] = conf.deployments[i].label;
+    for (var i = 0; i < this.noOfDeployments; i ++) {
+        deploymentLabels[i] = this.conf.deployments[i].label;
     }
     return deploymentLabels;
 }
 
-function initProxyPort() {
+Robin.prototype.initProxyPort = function () {
     var defaultPort = 80;
-    return conf.proxy_port || defaultPort; // "proxy_port" is optional in config.json.
+    return this.conf.proxy_port || defaultPort; // "proxy_port" is optional in config.json.
 }
 
-function setExpiryTime() {
+Robin.prototype.setExpiryTime = function () {
     var currentTimeInMillis = new Date().getTime();
-    var expires = parseInt(conf.expires);
+    var expires = parseInt(this.conf.expires);
     var expiryTime = new Date(currentTimeInMillis + expires);
     return expiryTime;
+}
+
+var robin = new Robin();
+
+var server = httpProxy.createServer(function (req, res, proxy) {
+    robin.cookies = new Cookies(req, res);
+    robin.receivedValue = robin.cookies.get(robin.cookieName);
+    if (robin.receivedValue == undefined) {
+        robin.target = robin.matchProxy(res);
+        robin.cookieValue = robin.labels[robin.domainIndex];
+        robin.cookies.set(robin.cookieName,robin.cookieValue, {expires: robin.expiryTime}, {domain: req.headers.host});
+        res.writeHead( 302, { "Location": "/" } )
+        return res.end();
+     } else {        
+        robin.target = robin.findDeployment(robin.receivedValue);
+     }
+    proxy.proxyRequest(req, res, robin.target);
+}).listen(robin.pport);
+
+
+Robin.prototype.matchProxy = function (res) {
+    var randomnumber= this.generateRandomNumber();
+    var depWeight;
+    for (var i = 0; i < this.noOfDeployments; i++) {
+        depWeight = this.conf.deployments[i].weight;
+        if (randomnumber < depWeight) {
+            this.domainIndex = i;
+            return this.addresses[i];
+        } else {
+            randomnumber = randomnumber - depWeight;
+        }
+    }
+    return this.defaultDeployment;
+}
+
+Robin.prototype.findDeployment = function (depLabel) {
+    for (var i = 0; i < this.noOfDeployments; i++) {
+        if (depLabel == this.labels[i]) {
+            return this.addresses[i];
+        } 
+    }
+    return this.defaultDeployment;
+}
+
+Robin.prototype.generateRandomNumber = function () {
+    var randomNumber = Math.floor(Math.random()*(this.maximumRandomNumber+1));
+    return randomNumber;
 }
