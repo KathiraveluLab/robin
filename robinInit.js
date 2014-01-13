39915a500a10c7396e6521f8f7783270ca67aa2c
Pradeeban <kk.pradeeban@gmail.com>
2014-01-13 17:40:44 +0000

    Adding the possibility to switch between the file and console outputs.

--- robinInit.js
+++ robinInit.js
@@ -6,6 +6,12 @@ var httpProxy = require('http-proxy/lib/node-http-proxy'),
 
 var configObject = etc().argv().env().etc();
 var robin = new Robin(configObject.toJSON());
+
+if (typeof process.argv[2] != 'undefined' && process.argv[2] === '-l') {
+    winston.remove(winston.transports.Console);
+    winston.add(winston.transports.File, {filename: process.argv[3]});
+}
+
 var robinWinston = new RobinWinston(robin, winston);
 
 var server = httpProxy.createServer(function (req, res, proxy) {
